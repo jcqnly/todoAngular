28bcf881948c4e901e12c4f012c847bcb096e884
jackie <jcqnly@gmail.com>
2018-10-20 16:31:46 -0700

    add checkbox

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -6,8 +6,12 @@ import { TodoItem } from '../interfaces/todo-item';
   selector: 'app-todo-item',
   template: `
   <div class="todo-item">
-    {{ item.title }}
-​
+​    <input type="checkbox"
+           class="todo-checkbox"
+           (click)="completeItem()"/>
+    <span class="todo-title" [ngClass]="{'todo-complete': item.completed}">
+      {{ item.title }}
+    </span>
     <button class="btn btn-red" (click)="removeItem()">
       remove
     </button>
@@ -19,13 +23,26 @@ import { TodoItem } from '../interfaces/todo-item';
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
   @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
+
   constructor() { 
   }
+
   removeItem() {
     this.remove.emit(this.item);
   }
+
   ngOnInit() {
     console.log(this.item);
   }
 
+  completeItem() {
+    this.item.completed = !this.item.completed
+    // this.update.emit({
+    //   item: this.item,
+    //   changes: {completed: !this.item.completed},
+    // });
+
+    console.log(this.item.completed);
+  }
 }
